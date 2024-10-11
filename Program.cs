f8d37043574588fadc38ed1c5c38b626c15f60c0
ISRAEL <jesus.ga@adoremeservices.com>
2024-10-11 00:55:58 -0700

    Primer Commit Leer Cvs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string ubicacionDelArchivo = "C:\\Users\\JesusIsraelGutierrez\\Desktop\\Apuntes\\Python\\Productos.csv";
+using (System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacionDelArchivo))
+{
+    string separador = ",";
+    string linea;
+    while ((linea = archivo.ReadLine()) != null)
+    {
+        string[] fila = linea.Split(separador);
+        string Descripcion = fila[0];
+        string Precio = fila[1];
+        string Existencia = fila[2];
+
+        // Cambia la forma de mostrar los valores
+        Console.WriteLine($"{Descripcion}, {Precio}, {Existencia}");
+    }
+}
